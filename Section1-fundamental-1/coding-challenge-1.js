b43394e08a204ee16c86981ebca6d8ef89f3136e
vihahuynh <huynhviha1703@gmail.com>
2021-09-23 19:09:58 +0700

    coding challenge 1

---
+++ Section1-fundamental-1/coding-challenge-1.js
@@ -0,0 +1,9 @@
+const weightMark = 78
+const heightMark = 1.69
+
+const weightJohn = 92
+const heightJohn = 1.95
+
+const bmiMark = weightMark / (heightMark * heightMark)
+const bmiJohn = weightJohn / (heightJohn * heightJohn)
+console.log(bmiMark > bmiJohn)

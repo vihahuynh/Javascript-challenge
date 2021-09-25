0c0b5c1f8775a076ff1e30442e355840511a0f68
vihahuynh <huynhviha1703@gmail.com>
2021-09-25 17:19:58 +0700

    Section 2: coding challenge 2

---
+++ Section2-fundamental-2/coding-challenge-2.js
@@ -0,0 +1,12 @@
+function calcTip(bill){
+    if (bill >= 50 && bill <= 300){
+        return bill * 0.15
+    }
+    return bill * 0.2
+}
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(total)
+

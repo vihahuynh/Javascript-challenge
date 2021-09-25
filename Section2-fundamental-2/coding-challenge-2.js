3c1343c12ed974f82183acacac13f22eb9e529b5
vihahuynh <huynhviha1703@gmail.com>
2021-09-25 17:23:58 +0700

    Section 2: coding challenge 2

--- Section2-fundamental-2/coding-challenge-2.js
+++ Section2-fundamental-2/coding-challenge-2.js
@@ -1,9 +1,4 @@
-function calcTip(bill){
-    if (bill >= 50 && bill <= 300){
-        return bill * 0.15
-    }
-    return bill * 0.2
-}
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
 
 const bills = [125, 555, 44]
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
